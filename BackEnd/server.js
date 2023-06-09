3d8065500ee8e88f80ef5d7516356b00a35b0fff
prabanchan10 <118807832+prabanchan10@users.noreply.github.com>
2023-06-09 13:09:17 +0530

    Update server.js

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -13,10 +13,10 @@ mongoose.connect("mongodb://127.0.0.1:27017/User", {
 });
 
 const SchemaUser = new mongoose.Schema({
-  email_id: { type: String, required: true },
-  pass: { type: String, required: true },
+  emailid: { type: String, required: true },
+  password: { type: String, required: true },
   userName: { type: String, required: true },
-  phone: { type: Number, required: true },
+  phone: { type: Number, required: true }
 });
 
 const User = mongoose.model("User", SchemaUser);
@@ -26,9 +26,9 @@ app.use(cors());
 
 app.post("/api/signup", async (req, res) => {
   try {
-    const { email_id, pass, userName, phone } = req.body;
-    const encryptedPassword = await bcrypt.hash(pass, 10);
-    await User.create({ email_id, pass: encryptedPassword, userName, phone });
+    const { emailid, password, userName, phone } = req.body;
+    const encryptedPassword = await bcrypt.hash(password, 10);
+    await User.create({ emailid, password: encryptedPassword, userName, phone });
     return res.status(201).json({ message: "User Created Successfully" });
   } catch (error) {
     return res.status(500).json({ message: "User Creation Failed" });
@@ -37,19 +37,19 @@ app.post("/api/signup", async (req, res) => {
 
 app.post("/api/login", async (req, res) => {
   try {
-    const { email_id, pass } = req.body;
-    const user = await User.findOne({ email_id });
+    const { emailid, password } = req.body;
+    const user = await User.findOne({ emailid });
     if (!user) {
       return res.status(401).json({ message: "User Name doesn't exist" });
     }
-    const isPasswordValid = await bcrypt.compare(pass, user.pass);
+    const isPasswordValid = await bcrypt.compare(password, user.password);
     if (!isPasswordValid) {
       return res
         .status(401)
         .json({ message: "User name or password is incorrect" });
     }
-    const jwt_token = jwt.sign({ email_id: user.email_id }, "Key");
-    res.json({ jwt_token });
+    const jwtToken = jwt.sign({ emailid: user.emailid }, "Key");
+    res.json({ jwtToken });
   } catch (error) {
     res.status(500).json({ message: "Login failed" });
   }
